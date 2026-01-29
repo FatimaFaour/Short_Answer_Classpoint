ad16ec2a30450054e3b6feebfc4101481a5f07f4
batoul-kanaan <aisofy224@gmail.com>
2026-01-29 16:16:29 +0200

    fixes

--- backend/main.py
+++ backend/main.py
@@ -15,16 +15,16 @@ app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), na
 
 @app.get("/")
 def join():
-    return FileResponse("static/join.html")
+    return FileResponse(os.path.join(BASE_DIR, "static", "join.html"))
+
 
 @app.get("/profile")
 def profile():
-    return FileResponse("static/profile.html")
+    return FileResponse(os.path.join(BASE_DIR, "static", "profile.html"))
 
 @app.get("/dashboard")
 def dashboard():
-    return FileResponse("static/dashboard.html")
-
+    return FileResponse(os.path.join(BASE_DIR, "static", "dashboard.html"))
 
 # ------------------ MODELS ------------------
 class JoinRequest(BaseModel):
--- teacher/connect.py
+++ teacher/connect.py
@@ -1,13 +1,55 @@
 import psycopg2
 
-def get_connection():
-    return psycopg2.connect(
-        dbname="short_ans_classpoint",
-        user="postgres",
-        password="ahmad1807",
-        host="localhost",
-        port="5432"
-    )
+import psycopg2
+from psycopg2 import sql
+
+DB_NAME = "short_ans_classpoint"
+USER = "postgres"
+PASSWORD = "123456"
+HOST = "localhost"
+PORT = "5432"
+
+def get_connection(dbname=DB_NAME):
+    try:
+        # Try connecting to the target database
+        conn = psycopg2.connect(
+            dbname=dbname,
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT
+        )
+        return conn
+    except psycopg2.OperationalError as e:
+        # If the database does not exist, create it
+        if "does not exist" in str(e):
+            # Connect to default database
+            conn = psycopg2.connect(
+                dbname="postgres",
+                user=USER,
+                password=PASSWORD,
+                host=HOST,
+                port=PORT
+            )
+            conn.autocommit = True
+            cur = conn.cursor()
+            cur.execute(sql.SQL("CREATE DATABASE {} TEMPLATE template0").format(
+                sql.Identifier(DB_NAME)
+            ))
+
+            cur.close()
+            conn.close()
+            # Now try connecting again
+            conn = psycopg2.connect(
+                dbname=DB_NAME,
+                user=USER,
+                password=PASSWORD,
+                host=HOST,
+                port=PORT
+            )
+            return conn
+        else:
+            raise
 
 def run_query(sql, params=None, fetch=False):
     conn = get_connection()
